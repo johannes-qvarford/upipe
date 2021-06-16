590cda1901cc4b07b43ff4862275635c3507e999
Johannes Qvarford <jq.email+gitlab@pm.me>
2021-06-16 17:50:55 +0200

    Can get "latest" videos and show multiple summaries.

--- upipe.rkt
+++ upipe.rkt
@@ -6,23 +6,14 @@
 
 (define (title) "(title)")
 (define (ext) "(ext)")
-
-(define (yt-video-summary index json-port)
-  (let* ([jsexpr (read-json json-port)]
-         [get (curry hash-ref jsexpr)])
-    (if (eof-object? jsexpr) #f
-        (format "~a\t~a\t~a\t~a" index (get 'title) (get 'channel) (get 'id)))))
-
-(define url "https://youtube.com/watch?v=M6EOBjxXUEM")
-
 (define *data-directory* (make-parameter "data"))
-
 (define (archive-directory) (format "~a/~a" (*data-directory*) "ytdl-archive.txt"))
+(define url "https://www.youtube.com/c/ChorpSawaySA/videos")
+(define (date-too-early) "today-2weeks")
+(define (videos-to-check) 5)
 
 (define (video-output-path-format channel)
   (format "~a/~a/%(title)s.%(ext)s" (*data-directory*) channel))
-
-(define (date-too-early) "today-2months")
 
 (define (channel-latest-yt-videos-port name url)
   (define output-string
@@ -30,16 +21,39 @@
       (λ () (system* 
                      "/usr/local/bin/youtube-dl"
                      "--dateafter" (date-too-early)
+                     "--playlist-end" (number->string (videos-to-check))
                      "--download-archive" (archive-directory)
                      "--dump-json"
                      "-o" (video-output-path-format name)
                      url))))
   (open-input-string output-string))
 
-(display (yt-video-summary 0 (channel-latest-yt-videos-port "Chorpsaway" url)))
+(define (json-stream json-port)
+  (define jsexpr (read-json json-port))
+  (if (eof-object? jsexpr) empty-stream
+      (stream-cons jsexpr (json-stream json-port))))
+
+(define (yt-video-summaries seq)
+  (for/list ([i (in-naturals)] [jsexpr seq])
+    (yt-video-summary i jsexpr)))
+
+(define (yt-video-summary index jsexpr)
+  (let*
+      ([get (curry hash-ref jsexpr)])
+    (format "~a  ~a  ~a  ~a  ~a" index (get 'title) (get 'channel) (get 'upload_date) (get 'id))))
+
+(define (latest-videos-command)
+  (define json-port (channel-latest-yt-videos-port "Chorpsaway" url))
+  (define summaries (yt-video-summaries (json-stream json-port)))
+  (for ([s summaries])
+    (display s)
+    (display #\newline)))
+
+(latest-videos-command)
 
 ; Goals:
-; Get today and yesterday's videos from channel
+; Fetch from list of subscriptions ("name" "url")
+; Store subscriptions in file, and read them when fetching latest videos.
 ; Download files given indexes. Load indexes from file overwritten by previous check.
 ; Get today and yesterday's videos from channel-file (subscriptions).
 ; Write down latest "check date" when calling "upipe done". This will be the lowest date, with no upper-date.
@@ -48,6 +62,7 @@
 ; pop from watchlist
 ; clear watchlist
 ; sort watchlist
+; Increase video amount window if we're able to fetch all of them.
 ; Create a web server. Search for video id on home page and redirect to page with above information.
 
 ; Do the same, but for channel (channel:blabla).  
